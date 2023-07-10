96b0c937b623c37d858dcd26e6e104dbfdc1a695
Fatima Najafi <najafifatima762@gmail.com>
2023-07-11 00:35:19 +0430

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpeg|jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
